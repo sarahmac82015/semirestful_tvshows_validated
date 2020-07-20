e6da3d7a265c45115693aeeec48a1bad2d0f5dd4
sarahmac82015 <65735476+sarahmac82015@users.noreply.github.com>
2020-07-19 19:10:44 -0500

    first commit

---
+++ shows/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .import views
+
+urlpatterns=[
+    path('shows/', views.index),
+    path('shows/new', views.new),
+    path('shows/create', views.create),
+    path('shows/<int:show_id>', views.show),
+    path('shows/<int:show_id>/edit', views.edit),
+    path('shows/<int:show_id>/update', views.update),
+    path('shows/<int:show_id>/delete', views.delete),
+]
